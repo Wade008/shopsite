ed7e63a3711ad224e49c35c45db0951346a453ee
Wade Doolan <wadedoolan@hotmail.com>
2019-12-04 20:43:53 +1000

    Change listener port for deployment

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.get("/contact", function(req,res){
 });
 
 // Listener
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started");
 });
